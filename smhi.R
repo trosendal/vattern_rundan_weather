4bc034d8703d525f3052096a07a75dde6aa6b634
Thomas Rosendal <thomas.rosendal@sva.se>
2018-06-09 21:51:10 +0200

    Update with new dates times and pace
    Also only ftp the data.js file no other files actually change

--- smhi.R
+++ smhi.R
@@ -66,41 +66,23 @@ map_format <- function(df){
 ##
 ##
 ## Get the race_times/weather info and format it:
-df <- vattern_pred(start = as.POSIXct("2017-06-16 19:38:00 CEST", tz = "Europe/Stockholm"),
-                   pace = 36)
+df <- vattern_pred(start = as.POSIXct("2018-06-16 12:20:00 CEST", tz = "Europe/Stockholm"),
+                   pace = 40.03)
 pts <- map_format(df)
 ## Write the points to json
 path_to_data <- svamap::write_data(pts)
 ## Plot them on a map:
 svamap::write_page(data = path_to_data,
-                   path = "~/Desktop/vattern/830/",
+                   path = "~/Desktop/vattern/730",
                    owntemplate = "map.html",
                    overwrite = TRUE,
                    browse = FALSE,)
-file.copy("vattern_poi.js", "~/Desktop/vattern/830/map/", overwrite = TRUE)
-file.copy("vattern_track.js", "~/Desktop/vattern/830/map/", overwrite = TRUE)
+file.copy("vattern_poi.js", "~/Desktop/vattern/730/map/", overwrite = TRUE)
+file.copy("vattern_track.js", "~/Desktop/vattern/730/map/", overwrite = TRUE)
 temp <- readLines("~/.epi-cloudftp_credentials")
-cred <- paste0("ftp://", temp[2], ":", temp[3], "@", temp[1], "/vattern/830/")
-files <- list.files("~/Desktop/vattern/830/map/", full.names = TRUE)
-for(i in files) {
-    ftpUpload(i, paste0(cred, basename(i)))
-}
-df <- vattern_pred(start = as.POSIXct("2017-06-16 19:38:00 CEST", tz = "Europe/Stockholm"),
-                   pace = 32)
-pts <- map_format(df)
-## Write the points to json
-path_to_data <- svamap::write_data(pts)
-## Plot them on a map:
-svamap::write_page(data = path_to_data,
-                   path = "~/Desktop/vattern/930/",
-                   owntemplate = "map.html",
-                   overwrite = TRUE,
-                   browse = FALSE,)
-file.copy("vattern_poi.js", "~/Desktop/vattern/930/map/", overwrite = TRUE)
-file.copy("vattern_track.js", "~/Desktop/vattern/930/map/", overwrite = TRUE)
-temp <- readLines("~/.epi-cloudftp_credentials")
-cred <- paste0("ftp://", temp[2], ":", temp[3], "@", temp[1], "/vattern/930/")
-files <- list.files("~/Desktop/vattern/930/map/", full.names = TRUE)
-for(i in files) {
-    ftpUpload(i, paste0(cred, basename(i)))
-}
+cred <- paste0("ftp://", temp[2], ":", temp[3], "@", temp[1], "/vattern/730/")
+files <- list.files("~/Desktop/vattern/730/map/", full.names = TRUE)
+ftpUpload(files[1], paste0(cred, basename(files[1])))
+## for(i in files) {
+##     ftpUpload(i, paste0(cred, basename(i)))
+## }
